b04a853b0ed04334cb591a46af2dd39a599d0aa1
raihan40 <86623443+raihan40@users.noreply.github.com>
2021-06-29 14:01:55 +0530

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -19,10 +19,10 @@ const append = (messege, position) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const messege = messegeInput.value;
-    do
+   if (messege!==null)
     {
     append(`${name}:${messege}`, 'right');
-    }while(messege!=="null")
+    }
     socket.emit('send', messege);
     messegeInput.value = '';
 
